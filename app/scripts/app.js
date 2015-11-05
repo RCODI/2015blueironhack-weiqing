50ac0129eff47a601b8417f1202dc6f6cec9e07a
Qing Wei <qingwei8801@gmail.com>
2015-11-05 09:50:02 -0500

    init project

---
+++ app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name 2015blueironhackWeiqingApp
+ * @description
+ * # 2015blueironhackWeiqingApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('2015blueironhackWeiqingApp', [
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
